3ea33730c804ad304b033ec1490b600a629084d8
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-30 19:03:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@ def game_hash
   home:{
     team_name:"Brooklyn Nets",
     colors:["Black", "White"],
-    players:
-    { player_name:"Alan Anderson" 
+    players:[{ 
+      player_name:"Alan Anderson" 
       number: 0,
       shoe: 16,
       points: 22,
@@ -53,13 +53,13 @@ def game_hash
       steals: 4,
       blocks: 11,
       slam_dunks: 1
-    } }
+    } ]
   }, 
   away:{
     team_name:"Charlotte Hornets",
     colors:["Turquoise", "Purple"],
-    players:{
-      player_name: "Jeff Adrien"=> {
+    players:[{
+      player_name: "Jeff Adrien"
       number: 4,
       shoe: 18,
       points: 10,
@@ -68,8 +68,8 @@ def game_hash
       steals: 2,
       blocks: 7,
       slam_dunks: 2
-    }
-      player_name:"Bismack Biyombo" => {
+    }, {
+      player_name:"Bismack Biyombo"
       number: 0,
       shoe: 16,
       points: 12,
@@ -78,8 +78,8 @@ def game_hash
       steals: 22,
       blocks: 15,
       slam_dunks: 10
-    }
-      player_name:"DeSagna Diop" => {
+    }, {
+      player_name:"DeSagna Diop"
       number: 2,
       shoe: 14,
       points: 24,
@@ -88,8 +88,8 @@ def game_hash
       steals: 4,
       blocks: 5,
       slam_dunks: 5
-    }
-      player_name:"Ben Gordon" => {
+    }, {
+      player_name:"Ben Gordon"
       number: 8,
       shoe: 15,
       points: 33,
@@ -98,8 +98,8 @@ def game_hash
       steals: 1,
       blocks: 1,
       slam_dunks: 0
-    }
-      player_name:"Kemba Walker" => {
+    }, {
+      player_name:"Kemba Walker"
       number: 33,
       shoe: 15,
       points: 6,
@@ -109,7 +109,7 @@ def game_hash
       blocks: 5,
       slam_dunks: 12
     }
-    }
+    ] }
   }
 }
   
