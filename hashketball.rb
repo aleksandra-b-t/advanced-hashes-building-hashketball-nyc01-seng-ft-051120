6d29c79885ce9e40f89dfbc96e3a4676ca534534
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-30 19:01:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
     team_name:"Brooklyn Nets",
     colors:["Black", "White"],
     players:
-    { player_name:"Alan Anderson" => {
+    { player_name:"Alan Anderson" 
       number: 0,
       shoe: 16,
       points: 22,
@@ -13,8 +13,8 @@ def game_hash
       steals: 3,
       blocks: 1,
       slam_dunks: 1
-    },
-      player_name:"Reggie Evans" => {
+    },{
+      player_name:"Reggie Evans"
       number: 30,
       shoe: 14,
       points: 12,
@@ -23,8 +23,8 @@ def game_hash
       steals: 12,
       blocks: 12,
       slam_dunks: 7
-    },
-      player_name:"Brook Lopez" => {
+    }, {
+      player_name:"Brook Lopez"
       number: 11,
       shoe: 17,
       points: 17,
@@ -33,8 +33,8 @@ def game_hash
       steals: 3,
       blocks: 1,
       slam_dunks: 15
-    }, 
-      player_name:"Mason Plumee" => {
+    }, {
+      player_name:"Mason Plumee"
       number: 1,
       shoe: 19,
       points: 26,
@@ -43,8 +43,8 @@ def game_hash
       steals: 3,
       blocks: 8,
       slam_dunks: 5
-    }, 
-      player_name:"Jason Terry" => {
+    }, {
+      player_name:"Jason Terry"
       number: 31,
       shoe: 15,
       points: 19,
